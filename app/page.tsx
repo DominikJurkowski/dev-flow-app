e8883b7dea9b7038c1ce4824ee3d82f87de1f21c
Dominik <dominik.jurkowski@noaignite.com>
2026-01-20 00:01:14 +0100

    feat: add theme, nav and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 
+
 const Home = () => {
   return (
     <div>
       <h1 className="h1-bold">Welcome to the Ultimate Next.js Course</h1>
       <h1 className="h1-bold font-inter">Welcome to the Ultimate Next.js Course (Inter Font)</h1>
+
+      
     </div>
   )
 }
