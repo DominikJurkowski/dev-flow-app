6e25ad66c588f2f21d060e6e87827f27cc18aba2
Dominik <dominik.jurkowski@noaignite.com>
2026-01-30 23:02:10 +0100

    feat: search params func

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,21 @@
+import LeftSidebar from '@/components/leftsidebar/LeftSidebar'
 import Navigation from '@/components/navigation/Navigation'
+import RightSidebar from '@/components/rightsidebar/RightSidebar'
 import React from 'react'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main>
-        <Navigation />
-        {children}
+    <main className="background-light850_dark100 realtive">
+      <Navigation />
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+        <RightSidebar />
+      </div>
     </main>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,84 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth, signOut } from '@/auth';
+import LocalSearch from '@/components/search/LocalSearch';
+import { Button } from '@/components/ui/button';
+import ROUTES from '@/constants/routes';
+import Link from 'next/link';
 
+const questions = [
+  {
+    _id: '1',
+    title: 'How to learn React?',
+    description: 'I want to learn React, can anyone help me?',
+    tags: [
+      { _id: '2', name: 'react' },
+      { _id: '3', name: 'javascript' },
+      { _id: '4', name: 'nextjs' },
+    ],
+    author: {
+      _id: '1',
+      name: 'John Doe',
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: '2',
+    title: 'How to learn Next.js?',
+    description: 'I want to learn Next.js, can anyone help me?',
+    tags: [
+      { _id: '5', name: 'react' },
+      { _id: '3', name: 'javascript' },
+      { _id: '4', name: 'nextjs' },
+    ],
+    author: {
+      _id: '2',
+      name: 'Jane Doe',
+    },
+    upvotes: 20,
+    answers: 10,
+    views: 200,
+    createdAt: new Date(),
+  },
+];
 
-const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
-
-  return (
-    <div>
-      <h1 className="h1-bold">Welcome to the Ultimate Next.js Course</h1>
-      <h1 className="h1-bold font-inter">Welcome to the Ultimate Next.js Course (Inter Font)</h1>
-
-    <form className="px-10 pt-[100px]" action={async () => {
-      "use server"
-      await signOut({
-        redirectTo: ROUTES.SIGN_IN,
-      });
-    }}>
-      <Button type="submit">Log Out</Button>
-    </form>
-    </div>
-  )
+interface SearchParamsProps {
+  searchParams: Promise<{ [key: string]: string }>;
 }
 
-export default Home
+const Home = async ({ searchParams }: SearchParamsProps) => {
+  // const session = await auth();
+
+  const { query = '' } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) => question.title.toLowerCase().includes(query?.toLowerCase()));
+
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button className="primary-gradient text-light-900! min-h-[46px] px-4 py-3" asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search for a question..."
+          otherClasses="flex-1"
+        />
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h2 key={question._id}>{question.title}</h2>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Home;
