3f47edc97353aece5397ed72084df7b0126f65ca
Dominik <dominik.jurkowski@noaignite.com>
2026-02-07 08:36:44 +0100

    feat: error handling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
-import LeftSidebar from '@/components/leftsidebar/LeftSidebar'
-import Navigation from '@/components/navigation/Navigation'
-import RightSidebar from '@/components/rightsidebar/RightSidebar'
-import React from 'react'
+import LeftSidebar from '@/components/leftsidebar/LeftSidebar';
+import Navigation from '@/components/navigation/Navigation';
+import RightSidebar from '@/components/rightsidebar/RightSidebar';
+import React from 'react';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,13 +11,13 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex">
         <LeftSidebar />
 
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
         <RightSidebar />
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
