605ec35d9fce262f5329219c274ab80852393386
Dominik <dominik.jurkowski@noaignite.com>
2026-02-10 16:37:46 +0100

    feat: api routes

---
+++ app/database.ts
@@ -0,0 +1,19 @@
+const tickets = [
+  {
+    id: 1,
+    name: 'Fix a bug in the app',
+    status: 'open',
+    type: 'refactor',
+  },
+  { id: 2, name: 'Add a new feature to the app', status: 'open', type: 'feature' },
+  { id: 3, name: 'Improve the performance of the app', status: 'open', type: 'performance' },
+  { id: 4, name: 'Add a new feature to the app', status: 'open', type: 'feature' },
+  { id: 5, name: 'Improve the performance of the app', status: 'open', type: 'performance' },
+  { id: 6, name: 'Add a new feature to the app', status: 'open', type: 'feature' },
+  { id: 7, name: 'Improve the performance of the app', status: 'open', type: 'performance' },
+  { id: 8, name: 'Add a new feature to the app', status: 'open', type: 'feature' },
+  { id: 9, name: 'Improve the performance of the app', status: 'open', type: 'performance' },
+  { id: 10, name: 'Add a new feature to the app', status: 'open', type: 'feature' },
+];
+
+export default tickets;
